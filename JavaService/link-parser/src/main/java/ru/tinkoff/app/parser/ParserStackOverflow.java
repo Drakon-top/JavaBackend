25ab58b0045a07b0cc3b4f7f13a86708d93be2f6
Drakon-top <kyzy77777@yandex.ru>
2023-03-16 00:34:42 +0300

    Create parserUrl 1.5

--- JavaService/link-parser/src/main/java/ru/tinkoff/app/parser/ParserStackOverflow.java
+++ JavaService/link-parser/src/main/java/ru/tinkoff/app/parser/ParserStackOverflow.java
@@ -5,13 +5,13 @@ import ru.tinkoff.app.url.UrlDataStackOverflow;
 
 public final class ParserStackOverflow implements ParserURL {
 
-    private final String QUESTION = "questions";
+    private final String QUESTIONS = "questions";
     private final String TYPE_URL = "stackoverflow.com";
 
     @Override
     public UrlData parseUrl(String url) {
         String[] args = url.split("/");
-        if (args.length > 4 && args[2].equals(TYPE_URL) && args[3].equals(QUESTION)) {
+        if (args.length > 4 && args[2].equals(TYPE_URL) && args[3].equals(QUESTIONS)) {
             try {
                 Integer id = Integer.parseInt(args[4]);
                 return new UrlDataStackOverflow(TYPE_URL, id);
