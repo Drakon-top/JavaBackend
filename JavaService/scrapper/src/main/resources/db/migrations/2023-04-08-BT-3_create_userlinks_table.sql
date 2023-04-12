25ba0310de243296d32549e13ac8a9181ee8530f
Drakon-top <kyzy77777@yandex.ru>
2023-04-12 13:55:00 +0300

    Refactor table user_links DB

--- JavaService/scrapper/src/main/resources/db/migrations/2023-04-08-BT-3_create_userlinks_table.sql
+++ JavaService/scrapper/src/main/resources/db/migrations/2023-04-08-BT-3_create_userlinks_table.sql
@@ -2,9 +2,10 @@
 
 --changeset ks:CRS-3_add_userlinks_sequence
 
-create table if not exists userlinks
+create table if not exists user_links
 (
-    id       serial primary key not null,
     user_id  bigint references client (chat_id),
-    links_id bigint references link (id)
+    links_id bigint references link (id),
+
+    primary key (user_id, links_id)
 );
