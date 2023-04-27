9f36acfaee68a77b612c47f6be2d6882ca89a627
Drakon-top <kyzy77777@yandex.ru>
2023-04-27 15:04:57 +0300

    Add RabbitMQConfiguration (Stage 8.2)

--- JavaService/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
+++ JavaService/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
@@ -2,11 +2,9 @@ package ru.tinkoff.edu.java.bot.configuration;
 
 
 import jakarta.validation.constraints.NotNull;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
-import ru.tinkoff.edu.java.bot.service.Bot;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotNull String test, String botToken) {}
+public record ApplicationConfig(@NotNull String test, String botToken, String queue, String exchange) {}
